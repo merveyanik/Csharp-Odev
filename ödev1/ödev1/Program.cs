7a49c4b87da40ac5f7849af1ed5ca7304b0c4e0c
merveyanik <merveynk52v@gmail.com>
2025-02-27 09:53:40 +0300

    Add files via upload

---
+++ ödev1/ödev1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ödev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            toyota toyota = new toyota();
+            toyota.ödev("toyota", "otomatik");
+
+            audi audi = new audi();
+            audi.ödev("audi", "düz");
+             
+            togg togg = new togg();
+            togg.ödev("togg", "otomatik");
+
+            mercedes mercedes = new mercedes();
+            mercedes.ödev("mercedes","otomatik veya düz");
+             
+            bmw bmw = new bmw();
+            bmw.ödev("bmw","düz");
+
+            porsche porsche = new porsche();
+            porsche.ödev("porsche", "otomatik");
+
+        }  
+
+    }
+}
---
+++ ödev1/ödev1/araba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ödev1
+{
+    public class araba
+    {
+        public string marka = null;
+        public string model = null;
+        public int kapisayisi = 0;
+        public int penceresayisi = 0;
+
+        public void ödev(string mirka, string vites)
+        {
+            Console.WriteLine(mirka + " " + vites + " viteslidir.");
+        }
+    }
+}
