eba9399219e132228c65899ba9089724d65e22b6
merveyanik <merveynk52v@gmail.com>
2025-03-23 15:20:32 +0300

    Add files via upload

---
+++ HesapMak/HesapMak/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HesapMak_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HesapMakinesi hesapMakinesi = new HesapMakinesi();
+            hesapMakinesi.SayilariAl();
+
+            Console.Write("Yapmak istediğiniz işlemi girin (+, -, *, /): ");
+            char islem = Convert.ToChar(Console.ReadLine());
+
+            hesapMakinesi.IslemiYap(islem);
+        }
+    }
+}
