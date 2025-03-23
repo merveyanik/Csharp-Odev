87e4dae20180d9be867e5ce84b0662aae9eba648
merveyanik <merveynk52v@gmail.com>
2025-03-23 14:53:41 +0300

    Add files via upload

---
+++ ödev4/ödev4/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Cryptography;
+using System;
+
+namespace ödev4
+{
+class Program
+        {
+            static void Main()
+            {
+                OrtalamaHesaplayici hesaplayici = new OrtalamaHesaplayici(); 
+                hesaplayici.SayilariAl(); // Kullanıcıdan sayıları alıyor.
+
+                double ortalama = hesaplayici.OrtalamaHesapla(); // Ortalamayı hesaplayacak kısım
+
+                Console.WriteLine($"Girilen sayıların ortalaması: {ortalama}");
+            }
+        }
+    }
+
